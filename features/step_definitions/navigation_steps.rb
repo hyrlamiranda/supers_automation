85e0233c8c1d7ee748af2b2457028276b6afba9f
Hyrla Miranda <hyrlamiranda@gmail.com>
2015-09-16 15:53:45 -0300

    putting comments in the code

--- features/step_definitions/navigation_steps.rb
+++ features/step_definitions/navigation_steps.rb
@@ -1,45 +1,48 @@
 #On step_definitions the 1º command open the loginPage
 Given(/^I am on SuperStars LoginPage$/) do
-  @browser.goto "http://staging-superstars.avenuecode.com:3030/"
+  #@browser.goto "http://staging-superstars.avenuecode.com:3030/"
+   @browser.goto "http://superstars.avenuecode.com/"
+
 end
-# Verify with valid 
+# Verify with valid email and password valid 
 When(/^I do the Login with valid data$/) do
     on(LoginPage).btnLogin_element.click
-    on(LoginPage).txtEmail = ' valid@mail.com'
+    on(LoginPage).txtEmail = 'valid@mail.com '
     on(LoginPage).btnNext_element.click
     sleep(2)
-    on(LoginPage).txtPassword = 'password'
+    on(LoginPage).txtPassword = ''
     on(LoginPage).btnSingin_element.click
 
 end
-
+# --userlist_element.visible?-- show if this element is True or false
 Then(/^I should see the SuperStars HomePage$/) do
   on(HomePage).userlist_element.visible?
 end
 
+# Verify with invalid email and password valid
 When(/^I do the Login with invalid username$/) do
     on(LoginPage).btnLogin_element.click
     on(LoginPage).txtEmail = 'invalid@mail.com'
     on(LoginPage).btnNext_element.click
     sleep(2)
-    on(LoginPage).txtPassword = 'password'
+    on(LoginPage).txtPassword = ''
     on(LoginPage).btnSingin_element.click
  end
-
+# --emailError_element.visible?-- show if this element is True or false
 Then(/^I shouldn't see SuperStars Password Page$/) do
       on(LoginPage).emailError_element.visible?
     #verify a way to validate text on span elements
 end
-
+# Verify with valid email and password invalid
 When(/^I do the Login with invalid password$/) do
     on(LoginPage).btnLogin_element.click
-    on(LoginPage).txtEmail = 'email@mail.com'
+    on(LoginPage).txtEmail = 'valid@mail.com'
     on(LoginPage).btnNext_element.click
     sleep(2)
     on(LoginPage).txtPassword = 'Invalidpassword'
     on(LoginPage).btnSingin_element.click
 end
-
+# --passwordError_element.text == 'Shows the error message'
 Then(/^I shouldn't see SuperStars HomePage$/) do
   on(LoginPage).passwordError_element.text == ' The email and password you entered don\'t match. '
 end
--- features/support/pages/Home_page.rb
+++ features/support/pages/Home_page.rb
@@ -2,7 +2,8 @@ class HomePage
 include PageObject
 
 
-page_url 'http://staging-superstars.avenuecode.com:3030/#!/'
+#page_url 'http://staging-superstars.avenuecode.com:3030/#!/'
+page_url 'http://superstars.avenuecode.com/#!/'
 
 
 	button(:btnNotNow, :class => 'md-warn md-hue-1 md-button md-default-theme')
