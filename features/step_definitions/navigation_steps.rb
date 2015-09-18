e33d474a75b0305ee825bf24ff52199750925434
Hyrla Miranda <hyrlamiranda@gmail.com>
2015-09-18 13:59:00 -0300

    improving my code

--- features/step_definitions/navigation_steps.rb
+++ features/step_definitions/navigation_steps.rb
@@ -6,45 +6,30 @@ Given(/^I am on SuperStars LoginPage$/) do
 end
 # Verify with valid email and password valid 
 When(/^I do the Login with valid data$/) do
-    on(LoginPage).btnLogin_element.click
-    on(LoginPage).txtEmail = 'valid@mail.com '
-    on(LoginPage).btnNext_element.click
-    sleep(2)
-    on(LoginPage).txtPassword = ''
-    on(LoginPage).btnSingin_element.click
-
+	on(LoginPage).logIn
 end
 # --userlist_element.visible?-- show if this element is True or false
 Then(/^I should see the SuperStars HomePage$/) do
-  on(HomePage).userlist_element.visible?
+	expect(on(HomePage).userlist_element.visible?).to be true
 end
 
 # Verify with invalid email and password valid
 When(/^I do the Login with invalid username$/) do
-    on(LoginPage).btnLogin_element.click
-    on(LoginPage).txtEmail = 'invalid@mail.com'
-    on(LoginPage).btnNext_element.click
-    sleep(2)
-    on(LoginPage).txtPassword = ''
-    on(LoginPage).btnSingin_element.click
+    on(LoginPage).InvalidUser
  end
+
 # --emailError_element.visible?-- show if this element is True or false
 Then(/^I shouldn't see SuperStars Password Page$/) do
-      on(LoginPage).emailError_element.visible?
+      on(LoginPage).emailError_element.visible?.to be true
     #verify a way to validate text on span elements
 end
 # Verify with valid email and password invalid
 When(/^I do the Login with invalid password$/) do
-    on(LoginPage).btnLogin_element.click
-    on(LoginPage).txtEmail = 'valid@mail.com'
-    on(LoginPage).btnNext_element.click
-    sleep(2)
-    on(LoginPage).txtPassword = 'Invalidpassword'
-    on(LoginPage).btnSingin_element.click
+    on(LoginPage).InvalidPass
 end
 # --passwordError_element.text == 'Shows the error message'
 Then(/^I shouldn't see SuperStars HomePage$/) do
-  on(LoginPage).passwordError_element.text == ' The email and password you entered don\'t match. '
+ 	expect(on(LoginPage).passwordError_element.text).to eq('The email and password you entered don\'t match.')
 end
     
 
--- features/support/pages/Login_page.rb
+++ features/support/pages/Login_page.rb
@@ -1,6 +1,6 @@
 class LoginPage
-include PageObject
-include DataMagic
+	include PageObject
+  	include DataMagic
 
 	page_url 'http://staging-superstars.avenuecode.com:3030/'
 
@@ -18,5 +18,30 @@ include DataMagic
 
 	button(:authorize_access,:id => 'submit_approve_access')
 
+	def logIn
+		self.btnLogin_element.click
+	    self.txtEmail = 'valid@gmail.com'
+	    self.btnNext_element.click
+	    sleep(2)
+	    self.txtPassword = 'validpassword'
+	    self.btnSingin_element.click
+	end
 
+	def InvalidUser
+		self.btnLogin_element.click
+    	self.txtEmail = 'invalid@mail.com'
+    	self.btnNext_element.click
+    	sleep(2)
+    	self.txtPassword = 'validpassword'
+    	self.btnSingin_element.click
+    end
+
+    def InvalidPass
+    	self.btnLogin_element.click
+    	self.txtEmail = 'valid@gmail.com'
+    	self.btnNext_element.click
+    	sleep(2)
+    	self.txtPassword = 'Invalidpassword'
+    	self.btnSingin_element.click
+    end
 end
